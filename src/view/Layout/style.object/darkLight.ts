7079290b541a0b1624540a45c2be1b26ab376610
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-08-01 14:09:20 -0300

    remove component

--- src/view/Layout/style.object/darkLight.ts
+++
@@ -1,6 +0,0 @@
-const dark = 'flex flex-row bg-Very-Dark-Desaturated-Blue  text-Very-Dark-Grayish-Blue-1 mx-auto w-[330px] md:w-[600px] h-[75px] xl:w-[660px] nt:w-[700px] relative top-[-7em] nt:top-[-8em] px-2 rounded-[4px]';
-const light = 'flex flex-row bg-Very-Light-Gray text-Very-Dark-Grayish-Blue mx-auto w-[330px] md:w-[600px] h-[75px]  xl:w-[660px] nt:w-[700px] relative top-[-7em] nt:top-[-8em]  px-2 rounded-[4px]'
-const MainL = ' relative top-[-3em] nt:top-[-6em] mx-auto  w-[330px] md:w-[600px]  xl:w-[660px] nt:w-[700px] bg-white rounded-[3px] px-[.1em] '
-const MainD = ' relative top-[-3em] nt:top-[-6em] mx-auto  w-[330px] md:w-[600px]  xl:w-[660px] nt:w-[700px] bg-Very-Dark-Desaturated-Blue rounded-[3px] px-[.1em] '
-const NavL = 'relative nt:hidden top-[-2em] w-[330px] md:w-[600px] px-20 mx-auto  xl:w-[660px] h-[60px] rounded-[3px] bg-white  flex flex-row justify-between items-center'
-const NavD = 'relative nt:hidden top-[-2em] w-[330px] md:w-[600px] px-20 mx-auto  xl:w-[660px] h-[60px] rounded-[3px] bg-Very-Dark-Desaturated-Blue  flex flex-row justify-between items-center'
